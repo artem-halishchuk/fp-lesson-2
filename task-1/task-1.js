ec82874261be90c6585263fd291b7201c198e6a0
Artem Halishchuk <artem.galishchuk@gmail.com>
2021-08-03 12:48:32 +0300

    task-1 added querySelector for elemets in and out

--- task-1/task-1.js
+++ task-1/task-1.js
@@ -1,4 +1,4 @@
-// Дано n секунд. Определить сколько это в часах и минутах;
+// Дано n секунд. Определить сколько это в часах и минутах
 // Пример
 // 7260с это 2 часа 1 мнута
 let secondEntered = parseInt(prompt("Введитее значение секунд для перевода в часы : минуты", 0));
@@ -12,4 +12,10 @@ else hoursPrint = "часов";
 if (minutes == 1) minutesPrint = "минута";
 else if ((minutes > 1) && (minutes < 5)) minutesPrint = "минуты";
 else minutesPrint = "минут";
-alert(secondEntered + "с это " + hours + " " + hoursPrint + " " + minutes + " " + minutesPrint);
+let result = secondEntered + " секунд это " + hours + " " + hoursPrint + " " + minutes + " " + minutesPrint;
+alert(result);
+
+let enteredValue = document.querySelector(".in");
+enteredValue.innerHTML = "Введенное значение "+ secondEntered + " секунд";
+let outValue = document.querySelector(".out");
+outValue.innerHTML = result;
